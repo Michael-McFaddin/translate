8e6e4ca30a643c3fb39d73a0cbf208cc79e22915
mike <52680887+Michael-McFaddin@users.noreply.github.com>
2021-02-10 13:57:47 -0500

    added button, field and usercreate components along with context logic

---
+++ src/components/Button.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
+class Button extends React.Component{
+  // static contextType = LanguageContext; 
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Voorleggen';
+  }
+
+  render() {
+    // console.log(this.context);
+    // const text = this.context === 'english' ? 'Submit' : 'Voorleggen'
+
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {/* {(value) => value === 'english' ? 'Submit' : 'Voorleggen' } */}
+          {value => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
+}
+
+export default Button;
