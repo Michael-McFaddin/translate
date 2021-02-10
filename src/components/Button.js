b4ae92c2b0e9e2b8cbaa8d85be0a2d6186e5089a
mike <52680887+Michael-McFaddin@users.noreply.github.com>
2021-02-10 16:21:41 -0500

    added colorcontext

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component{
   // static contextType = LanguageContext; 
+
+  // helper function to return value instead of having this in the render section
   renderSubmit(value) {
     return value === 'english' ? 'Submit' : 'Voorleggen';
   }
@@ -12,12 +15,16 @@ class Button extends React.Component{
     // const text = this.context === 'english' ? 'Submit' : 'Voorleggen'
 
     return (
-      <button className="ui button primary">
-        <LanguageContext.Consumer>
-          {/* {(value) => value === 'english' ? 'Submit' : 'Voorleggen' } */}
-          {value => this.renderSubmit(value)}
-        </LanguageContext.Consumer>
-      </button>
+      <ColorContext.Consumer>
+        {color =>
+          <button className={`ui button ${color}`}>
+          <LanguageContext.Consumer>
+            {/* {(value) => value === 'english' ? 'Submit' : 'Voorleggen' } */}
+            {value => this.renderSubmit(value)}
+          </LanguageContext.Consumer>
+        </button>
+        }
+      </ColorContext.Consumer>
     );
   }
 }
